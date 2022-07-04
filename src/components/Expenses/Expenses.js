3241b0f05f2d8d1f36716c8c3d358df9bbdd8b90
Nguyễn Thanh Nguyệt Phương <94954876+AmberPhuongNguyen@users.noreply.github.com>
2022-07-04 23:55:24 +0700

    add conditional return statement for filter- by- year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,6 +9,9 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setfilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <Card className="expenses">
       <ExpenseFilter
@@ -16,7 +19,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
       />
 
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
